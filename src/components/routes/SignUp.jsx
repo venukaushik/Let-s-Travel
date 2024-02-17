407bd539e5206cd51b58cccfcea24db094f0da55
venukaushik <venukaushik31@gmail.com>
2024-02-18 03:01:17 +0530

    .

--- src/components/routes/SignUp.jsx
+++ src/components/routes/SignUp.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import Navbar from "../Navbar/Navbar";
 import Hero from "../HeroSection/Hero";
 import SignUpHeroImg from "../../assets/9.jpg";
-import SignUp from "../SignUp/SignUp";
+import SignUpForm from "../SignUp/SignUpForm";
 import Footer from "../Footer/Footer";
 
 const Signup = () => {
   return (
     <div>
-      {/* <Navbar /> */}
       <Hero heroCName="about-hero" heroTextCName="hero-text" heroImg={SignUpHeroImg} heroTitle="SignUp" btnClassName="hide" url="/signup" />
-      <SignUp />
+      <SignUpForm />
       <Footer />
     </div>
   );
